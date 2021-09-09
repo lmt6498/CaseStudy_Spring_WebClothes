1ccd6b73e598f612dc284104c7ab64b6d82ee073
Akitoshi <luongminhtuan6498@gmail.com>
2021-09-09 11:18:01 +0700

    source database

---
+++ src/main/java/com/example/casestudy_spring_webclothes/CaseStudySpringWebClothesApplication.java
@@ -0,0 +1,13 @@
+package com.example.casestudy_spring_webclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudySpringWebClothesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudySpringWebClothesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casestudy_spring_webclothes/CaseStudySpringWebClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casestudy_spring_webclothes;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudySpringWebClothesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
